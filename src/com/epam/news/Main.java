26dccad634417f6d9bb4894289c3b493d2a2bfd2
Stepanov Sergey <mr.stepanov.sergey@gmail.com>
2012-10-23 23:03:24 +0300

    minimal changes
    no cool code here=)

--- src/com/epam/news/Main.java
+++ src/com/epam/news/Main.java
@@ -16,21 +16,5 @@ public class Main {
     public static void main(String[] args) {
 
 	ConnectionPool.init();
-	/*
-	 * DAOImpl dao = new DAOImpl(); News news = new News(); news =
-	 * dao.getById(1); System.out.println(news);
-	 */
-	for (int i = 0; i < 8; i++) {
-	    TestThread th = new TestThread("th" + i);
-	}
-	/*
-	 * try { Class.forName(ConnectionPool.driverClass); Connection
-	 * connection = DriverManager.getConnection( ConnectionPool.URI,
-	 * ConnectionPool.user, ConnectionPool.password); ResultSet rs =
-	 * connection.createStatement().executeQuery( "Select * from news");
-	 * while (rs.next()) { System.out.println(rs.getString("title")); } }
-	 * catch (SQLException | ClassNotFoundException e) {
-	 * e.printStackTrace(); }
-	 */
     }
 }
